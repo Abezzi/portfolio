5ab7bf88c14eca956d660d97bd6be9f58bf2dc51
abezzi <abezzi@outlook.cl>
2023-05-24 14:51:05 -0400

    feat(component): hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import Header from './components/Headers'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <main>
-      <h1>ola</h1>
       <Header />
-      {/*Header*/}
-      {/*hero*/}
+      <Hero />
       {/*about*/}
       {/*experience*/}
       {/*skills*/}
