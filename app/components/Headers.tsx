e6e43db7db0b2ffc123fe889f1c19257a89df93e
abezzi <abezzi@outlook.cl>
2023-05-24 13:27:21 -0400

    feat(component): header bar
    simple header bar with social media and contact icon

--- app/components/Headers.tsx
+++ app/components/Headers.tsx
@@ -3,13 +3,27 @@ import { SocialIcon } from 'react-social-icons'
 
 const Headers = () => {
   return (
-    <div>
-      <SocialIcon
-        url="http://github.com/abezzi"
-        fgColor="gray"
-        bgColor="transparent"
-      />
-    </div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      {/*left side*/}
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="http://github.com/abezzi"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+
+      {/*right side*/}
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Contact me</p>
+      </div>
+    </header>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Header from './components/Headers.tsx'
 export default function Home() {
   return (
     <main>
-      <h1>holi</h1>
       <Header />
       {/*Header*/}
       {/*hero*/}
