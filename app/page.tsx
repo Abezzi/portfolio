2eb7f1f867f24502ec3c2ee5efbe9604bd8f0b19
abezzi <abezzi@outlook.cl>
2023-05-24 14:41:05 -0400

    feat(component): header bar animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-import Header from './components/Headers.tsx'
+import Header from './components/Headers'
 
 export default function Home() {
   return (
     <main>
+      <h1>ola</h1>
       <Header />
       {/*Header*/}
       {/*hero*/}
