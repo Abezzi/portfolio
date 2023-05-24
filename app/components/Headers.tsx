47be3ff15b36aa222af1f6588bfba6e1dc05e7ee
abezzi <abezzi@outlook.cl>
2023-05-24 13:06:59 -0400

    first commit

---
+++ app/components/Headers.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+
+const Headers = () => {
+  return (
+    <div>
+      <SocialIcon
+        url="http://github.com/abezzi"
+        fgColor="gray"
+        bgColor="transparent"
+      />
+    </div>
+  )
+}
+
+export default Headers
